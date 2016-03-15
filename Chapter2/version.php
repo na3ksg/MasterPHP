5005d573f0b1ff3322677d215446ee15c1eebcaa
na3ksg <superkasago@yahoo.co.jp>
2016-03-15 19:39:59 +0900

    first commit

---
+++ Chapter2/version.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+
+
+
+-->
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>PHPのテスト</title>
+	</head>
+	<body>
+	    <?php
+	    // put your code here
+	    echo "PHPのバージョンは";
+	    echo PHP_VERSION;
+	    echo "だよ。<BR>";
+	    ?>
+	</body>
+</html>
