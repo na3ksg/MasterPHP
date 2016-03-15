221c32dfeb5b87cbfb5e7953ea724b98b0f42831
na3ksg <superkasago@yahoo.co.jp>
2016-03-15 20:52:00 +0900

    配列と連想配列

---
+++ Chapter2/array.php
@@ -0,0 +1,4 @@
+<meta charset="UTF8">
+<?php
+$month = array(1, 2, 3,);
+echo $month[0];
---
+++ Chapter2/associative_array.php
@@ -0,0 +1,10 @@
+<meta charset="UTF8">
+<?php
+$member = array("name" => "秋元真夏", "age" => 22, "tall" => 156);
+
+echo $member["name"];
+echo "<br>";
+echo $member["age"];
+echo "<br>";
+echo $member["tall"];
+
