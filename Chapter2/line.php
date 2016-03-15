30c65c7f7ff4c10f9703318246a7c4e9426c7ad6
na3ksg <superkasago@yahoo.co.jp>
2016-03-15 19:45:55 +0900

    処理中の行番号を出力する

---
+++ Chapter2/line.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>PHPのテスト</title>
+	</head>
+	<body>
+	    <?php
+	    // put your code here
+	    echo "ここは";
+	    echo __LINE__;
+	    echo "行目だよ。<BR>";
+	    ?>
+	</body>
+</html>
