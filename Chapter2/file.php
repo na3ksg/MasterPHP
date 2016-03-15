2638c72925fc38f77bd4c66a5dc23bf614881b5a
na3ksg <superkasago@yahoo.co.jp>
2016-03-15 19:43:41 +0900

    ファイル名を表示する

---
+++ Chapter2/file.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>PHPのテスト</title>
+	</head>
+	<body>
+	    <?php
+	    // put your code here
+	    echo "このファイルの名前は";
+	    echo __FILE__;
+	    echo "だよ。<BR>";
+	    ?>
+	</body>
+</html>
