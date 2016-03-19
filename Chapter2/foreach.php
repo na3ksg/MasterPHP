382f265bcfe6e52e85f8cc8a72b0d1cca8be6740
na3ksg <superkasago@yahoo.co.jp>
2016-03-19 23:19:08 +0900

    foreachの練習2

--- Chapter2/foreach.php
+++ Chapter2/foreach.php
@@ -2,6 +2,13 @@
 $member = array("name" => "田中太郎", "age" => 23, "tall" => 170);
 
 foreach ($member as $key => $value) {
-	echo "$key : $value";
+	if ($key == "name") {
+		$title = "名前";
+	} else if ($key == "age") {
+		$title = "年齢";
+	} else if ($key == "tall") {
+		$title = "身長";
+	}
+	echo "$title : $value";
 	echo "<br>";
 }
