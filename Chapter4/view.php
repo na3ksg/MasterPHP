57b464a1dd70984b96bf1de8689ed9ba25129579
na3ksg <superkasago@yahoo.co.jp>
2016-03-23 13:37:22 +0900

    プルダウンメニューのデータを取得

--- Chapter4/view.php
+++ Chapter4/view.php
@@ -6,11 +6,11 @@
 </head>
 <body>
    <?php 
-   if(isset($_POST["gender"]) && ($_POST["gender"] == "男" || $_POST["gender"] == "女")) {
-	   echo "性別：<br>";
-	   echo $_POST["gender"];
+   if($_POST["team"] != "") {
+	   echo 'Jリーグチーム：<br>';
+	   echo $_POST["team"];
    } else {
-	   echo "性別を選んでね！<br>";
+	   "選んでね！<br>";
    }
    ?>
 </body>
