dff4c011f0af343aa27a606965ecad9338a6543c
na3ksg <superkasago@yahoo.co.jp>
2016-03-23 12:37:35 +0900

    フォームからデータの送信

---
+++ Chapter4/view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHPのテスト</title>
+</head>
+<body>
+	<?php
+	echo $_POST["onamae"] . "ちゃん、こんにちは！";
+	?>
+    
+</body>
+</html>
