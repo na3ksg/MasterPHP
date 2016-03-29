a270d4fea2f179083a862fe5720738a53ed9017c
na3ksg <superkasago@yahoo.co.jp>
2016-03-29 23:46:48 +0900

    コード整形

--- Chapter4/session.php
+++ Chapter4/session.php
@@ -2,7 +2,7 @@
 session_start();
 
 $count = 1;
-if(isset($_SESSION["count"])) {
+if (isset($_SESSION["count"])) {
 	$count = $_SESSION["count"];
 	$count++;
 }
@@ -10,27 +10,27 @@ $_SESSION["count"] = $count;
 ?>
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>セッション変数のテスト</title>
-</head>
-<body>
-   セッション変数のテスト<br> 
-   <br>
-   <?php
-   if($count == 1) {
-	   ?>
-   	初めての訪問だよ<br>
-	<br>
-	セッション変数にデータがないよ<br>
-	このページをリロードしてね<br>
-	<?php
-   } else {
-	   ?>
-	君の訪問は<?=$count?>回目だよ！<br>
-	<?php
-   }
-   ?>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>セッション変数のテスト</title>
+	</head>
+	<body>
+		セッション変数のテスト<br> 
+		<br>
+		<?php
+		if ($count == 1) {
+			?>
+			初めての訪問だよ<br>
+			<br>
+			セッション変数にデータがないよ<br>
+			このページをリロードしてね<br>
+			<?php
+		} else {
+			?>
+			君の訪問は<?= $count ?>回目だよ！<br>
+			<?php
+		}
+		?>
+	</body>
 </html>
 
--- Chapter4/view.php
+++ Chapter4/view.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>PHPのテスト</title>
-</head>
-<body>
-   <?php
-   if(isset($_POST["team"])) {
-	   echo "好きなJリーグチームは以下のとおりです<br><br>";
-	   foreach ($_POST["team"] as $team) {
-		   echo $team;
-		   echo "<br>";
-	   } 
-   } else {
-	   echo "好きなJリーグチームはありません。";
-   }
-   ?>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>PHPのテスト</title>
+	</head>
+	<body>
+	    <?php
+	    if (isset($_POST["team"])) {
+		    echo "好きなJリーグチームは以下のとおりです<br><br>";
+		    foreach ($_POST["team"] as $team) {
+			    echo $team;
+			    echo "<br>";
+		    }
+	    } else {
+		    echo "好きなJリーグチームはありません。";
+	    }
+	    ?>
+	</body>
 </html>
